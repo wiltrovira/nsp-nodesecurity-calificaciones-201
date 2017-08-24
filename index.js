932736434dc8b0f17da7573ca25f7aa8356c646b
Wilt Rovira <wrovira@gmail.com>
2017-08-24 14:15:10 -0500

    Se agrega eslint a la funcionalidad y pruebas

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(request, response){
-    response.send("Hola, mundo. Para calcular promedio escribe http://localhost:3000/notafinal?notas=90,80,70,60,50");
+	response.send("Hola, mundo. Para calcular promedio escribe http://localhost:3000/notafinal?notas=90,80,70,60,50");
 });
 
 app.get("/notafinal", function(request, response){
-    var notas = request.query.notas.split(",");
-    for(var i=0; i < notas.length; i++){
-        libro.agregarCalificacion(parseInt(notas[i]));
-    }
-    var promedio = libro.calcularPromedio();
-    var notaFinal = libro.calcularNotaFinal();
+	var notas = request.query.notas.split(",");
+	for(var i=0; i < notas.length; i++){
+		libro.agregarCalificacion(parseInt(notas[i]));
+	}
+	var promedio = libro.calcularPromedio();
+	var notaFinal = libro.calcularNotaFinal();
 
-    response.send("Su promedio es: " + promedio + ". Su nota final es:" + notaFinal )
-})
+	response.send("Su promedio es: " + promedio + ". Su nota final es:" + notaFinal );
+});
 app.listen(3000);
 console.log("Esperando conexiones...");
